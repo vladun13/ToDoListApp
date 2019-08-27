cc9d864968a69adcbe4ce7bbd47449db83537db2
vladun13 <nemyrovsky1311vlad@gmail.com>
2019-08-27 18:05:22 +0300

    Initial Commit

---
+++ ToDoListApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoListApp
+//
+//  Created by me on 27/08/2019.
+//  Copyright © 2019 Vlad Nemyrovsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
